07f827d04b725c11b553e337c37c1ee2388e2db8
chanj14ebsco <chandlerjones@ebsco.com>
2022-06-07 14:59:30 -0700

    First commit with Spring Boot scaffolding

---
+++ src/main/java/com/truetoyourshelf/truetoyourselfapi/TrueToYourselfApiApplication.java
@@ -0,0 +1,13 @@
+package com.truetoyourshelf.truetoyourselfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueToYourselfApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrueToYourselfApiApplication.class, args);
+    }
+
+}
